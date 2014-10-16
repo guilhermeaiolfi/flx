9aaf924c78d316e986dc0330aa86bb15abf6a582
Guilherme R. Aiolfi <gradinf@gmail.com>
2014-10-16 20:21:04 -0300

    Changed url to genURL for the ember helper

--- flx.js
+++ flx.js
@@ -1,6 +1,6 @@
 var Application = require("lib/application");
 var Router = require("lib/routing/router");
-var Router = require("lib/adapters/ember/layout_manager");
+var EmberLayoutManager = require("lib/adapters/ember/layout_manager");
 var lib = {};
 lib.Application = Application;
 lib.Router = Router;
